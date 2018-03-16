35a7f84f2e5c5797bd9fed6a875e247a65672cd7
Giuseppe Valenti <>
2018-03-16 17:55:06 +0100

    * added route for lstReq

--- routes.js
+++ routes.js
@@ -5,5 +5,6 @@ const cardController = require('./controllers/cardController');
 const trelloController = require('./controllers/trelloController');
 
 router.all('/hooks', trelloController.newHook);
+router.all('/lastReq', trelloController.lastReq);
 
 module.exports = router;
